cc8cf93e1ea2e5b625b45a8e871788f02306668a
say8hi <maks.mir@yahoo.com>
2023-12-23 20:50:03 +0600

    Очистка ветки

--- tg_bot/models/database.py
+++
@@ -1,7 +0,0 @@
-from tg_bot.utils.db_commands import DatabaseCommands
-
-
-class Database:
-    users = DatabaseCommands("users")
-    notifications = DatabaseCommands("notifications")
-    time_zones = DatabaseCommands("time_zones")
